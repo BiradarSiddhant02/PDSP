677cbe0a6975d88e7de8db764fb6587f73930576
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2023-02-13 23:27:48 +0530

    new file:   Algorithms/FFT.py

---
+++ Algorithms/FFT.py
@@ -0,0 +1,30 @@
+import cmath
+import matplotlib.pyplot as plt
+
+# Function to calculate the DIT-FFT
+def DIT_FFT(x):
+    N = len(x)
+    if N <= 1: return x
+    even = DIT_FFT(x[0::2])
+    odd =  DIT_FFT(x[1::2])
+    T= [cmath.exp(-2j*cmath.pi*k/N)*odd[k] for k in range(N//2)]
+    return [even[k] + T[k] for k in range(N//2)] + [even[k] - T[k] for k in range(N//2)]
+
+def DIFFT(x):
+    N = len(x)
+    if N <= 1: return x
+    even = DIFFT(x[0::2])
+    odd =  DIFFT(x[1::2])
+    T= [cmath.exp(2j*cmath.pi*k/N)*odd[k] for k in range(N//2)]
+    return [even[k] + T[k] for k in range(N//2)] + [even[k] - T[k] for k in range(N//2)]
+
+XK = DIT_FFT([1,2,3,4,5,6,7,8])
+for i in range(8):
+    print(XK[i])
+print()
+
+XK = DIT_FFT([1,2,3,4,5,6,7,8])
+for i in range(8):
+    print(XK[i])
+print()
+    
